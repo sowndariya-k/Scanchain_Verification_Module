d8d2c7444b073e41b3448ee250e2aee75dfa3e91
Kavinkumar <kavinkumar442005@gmail.com>
2025-04-24 20:05:46 +0530

    update

--- js/security.js
+++ js/security.js
@@ -12,11 +12,19 @@ document.addEventListener('keydown', function(e) {
 });
 
 // Warn before leaving
-window.onbeforeunload = function() {
-  return "Leaving this page might interrupt the voting process. Are you sure you want to proceed?";
+window.onbeforeunload = function(event) {
+  // Standard practice to check if the event has been prevented
+  if (!event.defaultPrevented) {
+    return "Leaving this page might interrupt the voting process. Are you sure you want to proceed?";
+  }
 };
 
-// Request full screen on first click anywhere
+// Function to check if the document is in fullscreen
+function isFullscreen() {
+  return !!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
+}
+
+// Request full screen
 function openFullscreen() {
   const elem = document.documentElement;
   if (elem.requestFullscreen) {
@@ -30,10 +38,17 @@ function openFullscreen() {
   }
 }
 
+// Event listener for the first click to enter fullscreen
 function onFirstClick() {
   openFullscreen();
-  // Remove listener after first click
-  document.removeEventListener('click', onFirstClick);
+  document.removeEventListener('click', onFirstClick); // Remove listener after first click
 }
 
 document.addEventListener('click', onFirstClick);
+
+// Event listener to re-enter fullscreen if focus is regained and not already in fullscreen
+document.addEventListener('focus', () => {
+  if (!isFullscreen()) {
+    openFullscreen();
+  }
+});
